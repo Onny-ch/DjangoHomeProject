a82c094cdf7ae43ceac5f0c661c15c9407db8d12
Andrey Osipov <bezy4uy@gmail.com>
2025-07-08 14:00:51 +0300

    added models Product and Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование продукта')
+    description = models.TextField(verbose_name='Описание продукта')
+    image = models.ImageField(upload_to='images', verbose_name='Изображение')
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    purchase_price = models.FloatField(verbose_name='Цена за покупку')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['category', 'purchase_price', 'name', 'created_at',]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование категории')
+    description = models.TextField(verbose_name='Описание категории')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name',]
