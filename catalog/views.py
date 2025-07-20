ae3615d3a8ce648fa8f7babc0a9712b979b9b644
Andrey Osipov <bezy4uy@gmail.com>
2025-07-20 20:50:42 +0300

    added changes for urls.py and views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from catalog.models import Product
 
 
 def home(request):
@@ -13,3 +15,15 @@ def contacts(request):
         message = request.POST.get("message")
         return HttpResponse(f"Ваше сообщение получено, {name}. Мы свяжемся с вами!")
     return render(request, 'contacts.html')
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
