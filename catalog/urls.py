e74973cba485a9b326a101b6a6481629d361020e
Andrey Osipov <bezy4uy@gmail.com>
2025-07-26 18:59:18 +0300

    recreated urls for CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_list, product_detail
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, HomeView, ContactsView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('products_list/', products_list, name='products_list'),
-    path('product_detail/<int:pk>/', product_detail, name='product_detail')
+    path('', HomeView.as_view(), name='home_view'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product_list/', ProductListView.as_view(), name='product_list'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_confirm_delete'),
 ]
