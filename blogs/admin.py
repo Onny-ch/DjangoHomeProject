4e8cd6bc7dc3aee013555556fc28d3c73af64708
Andrey Osipov <bezy4uy@gmail.com>
2025-07-26 19:00:44 +0300

    added blogs app and blogs model

---
+++ blogs/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Blogs
+
+
+@admin.register(Blogs)
+class BlogsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'preview', 'created_at', 'publication_sign', 'views_counter',)
+    list_filter = ('publication_sign', 'created_at',)
+    search_fields = ('title', 'created_at',)
