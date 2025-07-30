94e5d95e6df91a10a1d2d7038fe616e4f6cae391
Andrey Osipov <bezy4uy@gmail.com>
2025-07-30 09:15:39 +0300

    added form classes

--- catalog/views.py
+++ catalog/views.py
@@ -9,7 +9,35 @@ from django.views.generic import (
     TemplateView,
 )
 
-from catalog.models import Product
+from catalog.models import Product, Category
+from catalog.forms import ProductForm, CategoryForm
+
+
+class CategoryListView(ListView):
+    model = Category
+    context_object_name = "categories"
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = "catalog/category_form.html"
+    success_url = reverse_lazy("catalog:category_list")
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    success_url = reverse_lazy("catalog:category_list")
+
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy("catalog:category_list")
 
 
 class ProductListView(ListView):
@@ -20,32 +48,34 @@ class ProductDetailView(DetailView):
     model = Product
 
 
-class HomeView(TemplateView):
-    template_name = "catalog/home.html"
-
-
-class ContactsView(TemplateView):
-    template_name = "catalog/contacts.html"
-
-    def post(self, request, *args, **kwargs):
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        return HttpResponse(f"Ваше сообщение получено, {name}. Мы свяжемся с вами!")
-
-
 class ProductCreateView(CreateView):
     model = Product
-    fields = ("name", "description", "image", "category", "price")
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
     success_url = reverse_lazy("catalog:product_list")
 
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ("name", "description", "image", "category", "price")
+    form_class = ProductForm
     success_url = reverse_lazy("catalog:product_list")
 
 
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy("catalog:product_list")
+
+
+class HomeView(TemplateView):
+    template_name = "catalog/home.html"
+
+
+class ContactsView(TemplateView):
+    template_name = "catalog/contacts.html"
+    #form_class = ContactForm
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        return HttpResponse(f"Ваше сообщение получено, {name}. Мы свяжемся с вами!")
