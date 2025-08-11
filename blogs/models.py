58c3595be1dedd44257074bb0615ada89184d29c
Onny-ch <bezy4uy@gmail.com>
2025-08-12 02:17:52 +0300

    recreate models for blogs and catalog

---
+++ blogs/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+from users.models import User
+
+
+class Blogs(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Заголовок",
+    )
+    content = models.TextField(
+        verbose_name="Содержимое записи",
+    )
+    preview = models.ImageField(
+        upload_to="previews",
+        verbose_name="Изображение",
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+    )
+    publication_sign = models.BooleanField(
+        default=True
+    )
+    views_counter = models.PositiveIntegerField(
+        verbose_name="Счетчик просмотров",
+        help_text="Укажите количество просмотров",
+        default=0,
+    )
+    creator = models.ForeignKey(User, verbose_name='Создатель', help_text='Укажите создателя публикации', blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+        ordering = [
+            "created_at",
+        ]
---
+++ catalog/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = [
+            "name",
+        ]
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта",
+    )
+    image = models.ImageField(
+        upload_to="images",
+        verbose_name="Изображение",
+        null=True,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name="Категория продукта",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    price = models.FloatField(verbose_name="Цена продукта")
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+    )
+    updated_at = models.DateField(
+        auto_now=True,
+        verbose_name="Дата последнего изменения",
+    )
+    views_counter = models.PositiveIntegerField(
+        verbose_name="Счетчик просмотров",
+        help_text="Укажите количество просмотров",
+        default=0,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = [
+            "name",
+        ]
