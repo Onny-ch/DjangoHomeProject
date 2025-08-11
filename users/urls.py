e65b39b67500f14e4f00575839c57390f0114b64
Onny-ch <bezy4uy@gmail.com>
2025-08-12 02:15:11 +0300

    added new users app

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from users.apps import UsersConfig
+from users.views import RegisterView, email_verification
+
+# from users.views import
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='catalog:home_view'), name='logout'),
+    path('email_confirm/<str:token>/', email_verification, name='email_confirm')
+]
