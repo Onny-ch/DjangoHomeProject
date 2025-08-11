dd8e52464c47b83b7d1c41465b91db6418adc249
Onny-ch <bezy4uy@gmail.com>
2025-08-12 02:16:20 +0300

    recreate urls

--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blogs/', include('blogs.urls'))
+    2. Add a URL to urlpatterns:  path('users/', include('users.urls'))
 """
 
 from django.conf import settings
@@ -23,5 +23,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
-    path("blogs/", include("blogs.urls", namespace="blogs")),
+    path("", include("blogs.urls", namespace="blogs")),
+    path("users/", include("users.urls", namespace="users")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
