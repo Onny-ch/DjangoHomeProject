dee8ca65d42bd9a84741e777f8cb1d66955cdf29
Onny-ch <bezy4uy@gmail.com>
2025-08-12 02:15:24 +0300

    added forms for blog

---
+++ blogs/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+from blogs.models import Blogs
+
+
+class BlogsForm(forms.ModelForm):
+    class Meta:
+        model = Blogs
+        fields = ["title", "content", "preview",]
+
+
+    def __init__(self, *args, **kwargs):
+        super(BlogsForm, self).__init__(*args, **kwargs)
+
+        self.fields["title"].widget.attrs.update(
+            {
+                "class": "form-control",
+                "placeholder": "Введите заголовок",
+            }
+        )
+
+        self.fields["content"].widget.attrs.update(
+            {
+                "class": "form-control",
+                "placeholder": "Добавьте содержание записи",
+            }
+        )
+
+        self.fields["preview"].widget.attrs.update(
+            {
+                "class": "form-control",
+            }
+        )
