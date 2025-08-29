75f8c538138d7aa4755cf93bcc7f1658da829c48
Onny-ch <bezy4uy@gmail.com>
2025-08-29 14:33:12 +0300

    added automatic adding of product owner and display of the corresponding product editing form

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -11,7 +12,7 @@ from django.views.generic import (
 )
 
 from catalog.models import Product, Category
-from catalog.forms import ProductForm, CategoryForm
+from catalog.forms import ProductForm, CategoryForm, ProductModeratorForm
 
 
 class CategoryListView(ListView):
@@ -55,11 +56,26 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy("catalog:product_list")
 
+    def form_valid(self, form):
+        product = form.save()
+        user = self.request.user
+        product.owner = user
+        product.save()
+        return super().form_valid(form)
+
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:product_list")
+
+    def get_form_class(self):
+        user = self.request.user
+        if user == self.object.owner:
+            return ProductForm
+        if user.has_perm("catalog.can_unpublish_product"):
+            return ProductModeratorForm
+        raise PermissionDenied
 
 
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
